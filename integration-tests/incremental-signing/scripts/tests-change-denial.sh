95298f306f85f811908a27e4cfa088ba4e4da978
Philip-NLnetLabs <philip@nlnetlabs.nl>
2026-03-31 16:27:31 +0200

    Add tests for APEX records and switching between NSEC and NSEC3. (#554)

--- integration-tests/incremental-signing/scripts/tests-change-denial.sh
+++ integration-tests/incremental-signing/scripts/tests-change-denial.sh
@@ -5,8 +5,32 @@ CASCADE="cargo run --bin cascade"
 KEY=$PWD/keys/Kexample.+015+02835.key
 for zonemd in '' # zmd384
 do
-	for m in nsec nsec3 nsec3-opt-out
+	for m in nsec3/nsec nsec3-opt-out/nsec nsec/nsec3 nsec3-opt-out/nsec3 nsec/nsec3-opt-out nsec3/nsec3-opt-out
 	do
+		case $m in
+		nsec/*)
+			denial_before=nsec
+		;;
+		nsec3/*)
+			denial_before=nsec3
+		;;
+		nsec3-opt-out/*)
+			denial_before=nsec3-opt-out
+		;;
+		esac
+
+		case $m in
+		*/nsec)
+			denial_after=nsec
+		;;
+		*/nsec3)
+			denial_after=nsec3
+		;;
+		*/nsec3-opt-out)
+			denial_after=nsec3-opt-out
+		;;
+		esac
+
 		case "$zonemd" in
 		'')
 			policy="$m"
@@ -16,10 +40,13 @@ do
 		;;
 		esac
 
-		for test in 1 2 3 5
+		for test in 1 2 3
 		do
+			echo test $test denial $m $zonemd
 			cp zones/incremental-signing-test${test}-input1.zone example.in
-			$CASCADE zone add --source $PWD/example.in --policy $policy example --import-csk-file $KEY
+			cp policies/$denial_before.toml policies/work.toml
+			$CASCADE policy reload
+			$CASCADE zone add --source $PWD/example.in --policy work example --import-csk-file $KEY
 
 			# Wait for first version to be signed.
 			for i in 1 2 3 4 5 6 7 8 9 10
@@ -36,6 +63,9 @@ do
 				    exit 1
 				}
 			cp zones/incremental-signing-test${test}-input2.zone example.in
+			cp policies/$denial_after.toml policies/work.toml
+			$CASCADE policy reload
+
 			$CASCADE zone reload example
 			for i in 1 2 3 4 5 6 7 8 9 10
 			do
@@ -51,7 +81,7 @@ do
 				    exit 1
 				}
 
-			cp reference-output/incremental-signing-test${test}-input2.zone.${m}.signed.sorted reference.output.sorted
+			cp reference-output/incremental-signing-test${test}-input2.zone.${denial_after}.signed.sorted reference.output.sorted
 			dig @127.0.0.1 -p 8053 example axfr |
 			    egrep -v '^;|^$' | sort -u > output.sorted
 			diff -w -u output.sorted reference.output.sorted
